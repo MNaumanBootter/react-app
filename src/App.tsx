81d46feab1351b74ed304ceeb5da79cc72b80b5c
Nauman Bootter <mnaumanbootter@gmail.com>
2024-01-26 19:38:38 +0500

    use props to make ListGroup generic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = ["New York", "London", "Paris", "Tokyo"];
+
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading="Cities"></ListGroup>
     </div>
   );
 }
