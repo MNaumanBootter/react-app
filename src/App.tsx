c9f527ccad7d9d5d3ebabd09f9f2a653b6ce3873
Nauman Bootter <mnaumanbootter@gmail.com>
2024-06-24 16:35:15 +0500

    test css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["Lahore", "Islamabad"];
+  const handleSelectItem = (item: any) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      {/* <ListGroup
+      <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      ></ListGroup> */}
+      ></ListGroup>
       {alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
       )}
