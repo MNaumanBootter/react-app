7b793ef5ca6cae50d4055728a1fdcb94d3fcb598
Nauman Bootter <mnaumanbootter@gmail.com>
2024-08-13 14:47:57 +0500

    Test updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,22 @@ import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+  const [drink, setDrink] = useState({
+    title: "Latte",
+    price: 5,
+  })
   const items = ["Lahore", "Islamabad"];
   const handleSelectItem = (item: any) => {
+    setDrink({
+      ...drink,
+      price: 6,
+    });
     console.log(item);
   };
 
   return (
     <div>
+      {drink.price}
       <ListGroup
         items={items}
         heading="Cities"
