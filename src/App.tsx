d087ab711bc49ec103ddc6b450f50bcd3dd5efee
Nauman Bootter <mnaumanbootter@gmail.com>
2024-08-26 13:28:52 +0500

    Add game state update excercise

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "Smith" } });
+  };
+
   return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-    </div>
+    <>
+      <div>{game.player.name}</div>
+      <button onClick={handleClick}>Update name</button>
+    </>
   );
 }
 
