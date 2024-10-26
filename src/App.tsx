f5a79f92c060399f3d477fdb8d85f7f5ec34e6c7
Nauman Bootter <mnaumanbootter@gmail.com>
2024-10-26 08:36:18 +0500

    Add Expense Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import Expense from "./expense-tracker/expense";
 
 function App() {
-  return <Form />;
+  const [selectedCategory, setselectedCategory] = useState("");
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        ></ExpenseForm>
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setselectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
 export default App;
