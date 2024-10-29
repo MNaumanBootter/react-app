f1cf45116495a785539cf84fd6ca22421d205e7e
Nauman Bootter <mnaumanbootter@gmail.com>
2024-10-29 12:11:33 +0500

    Test useEffect with dependencies in ProductList comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import Expense from "./expense-tracker/expense";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const [selectedCategory, setselectedCategory] = useState("");
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
+  const [category, setCategory] = useState("");
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        ></ExpenseForm>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setselectedCategory(category)}
-        ></ExpenseFilter>
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category}></ProductList>
     </div>
   );
 }
