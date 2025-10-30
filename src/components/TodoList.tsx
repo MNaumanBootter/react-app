7b91893d36a63ce37862f0d8414ef35faf005a6b
Nauman Bootter <mnaumanbootter@gmail.com>
2025-10-30 19:47:58 +0500

    Add loader from react-query

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,13 +11,18 @@ interface Todo {
 const TodoList = () => {
   const fetchTodos = () =>
     axios
-      .get<Todo[]>("https://xjsonplaceholder.typicode.com/todos")
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
+  if (isLoading) return <div className="spinner-border"></div>;
 
   if (error) return <p>{error.message}</p>;
 
